f91342eade1b44527a2ca96cc0813c2f875cb6d1
NatdanaiPuthom <natdanai.puthom1997@gmail.com>
2023-12-06 23:09:10 +0100

    Updated engine name to v5.0 preparing new task

--- Source/Engine/engine.cpp
+++ Source/Engine/engine.cpp
@@ -101,7 +101,7 @@ std::unique_ptr<HWND> Engine::SetupMainWindow(HINSTANCE& hInstance, const int aW
 	std::unique_ptr<HWND> hwnd = std::make_unique<HWND>();
 	*hwnd = CreateWindow(
 		L"Natdanai",
-		L"SimpleEngine v4.7",
+		L"SimpleEngine v5.0",
 		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
