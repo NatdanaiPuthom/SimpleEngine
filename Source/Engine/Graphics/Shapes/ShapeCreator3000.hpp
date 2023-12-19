5a808e49add172a8915dd1c1075455fc38b2d44e
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2023-12-19 14:31:14 +0100

    Added Cat.dds and Adjusted pyramid to be scalable

--- Source/Engine/Graphics/Shapes/ShapeCreator3000.hpp
+++ Source/Engine/Graphics/Shapes/ShapeCreator3000.hpp
@@ -4,8 +4,8 @@
 namespace Shape
 {
 	MeshData CreateTerrain();
-	MeshData CreatePyramid();
-	MeshData CreateCube(const SimpleUtilities::Vector3f aSize = {0.5f, 0.5f , 0.5f});
-	MeshData CreateSkyBox(const SimpleUtilities::Vector3f& aSize = {100.0f, 100.0f, 100.0f});
+	MeshData CreatePyramid(const SimpleUtilities::Vector3f aSize = { 1.0f , 1.0f, 1.0f });
+	MeshData CreateCube(const SimpleUtilities::Vector3f aSize = { 0.5f, 0.5f , 0.5f });
+	MeshData CreateSkyBox(const SimpleUtilities::Vector3f& aSize = { 100.0f, 100.0f, 100.0f });
 	MeshData CreateDirectionalLight();
 }
