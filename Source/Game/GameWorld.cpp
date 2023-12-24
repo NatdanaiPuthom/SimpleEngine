3a81e7b2d3e575b814ead778f597077a12bbe719
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2023-12-24 22:02:55 +0100

    Ok getting more and more stuff back

--- Source/Game/GameWorld.cpp
+++ Source/Game/GameWorld.cpp
@@ -1,5 +1,4 @@
 #include "Game/stdafx.h"
-#include "Engine/Graphics/Renderer/Renderer.hpp"
 #include "Game/GameWorld.hpp"
 #include "Game/Managers/ImGuiManager/ImGuiManager.hpp"
 #include "Game/Managers/ImGuiManager/Tools/MeshTool.hpp"
@@ -16,59 +15,40 @@ GameWorld::~GameWorld()
 void GameWorld::Init()
 {
 	Renderer* renderer = SimpleGlobal::GetRenderer();
+	ModelFactory* modelFactory = SimpleGlobal::GetModelFactory();
 
 	myImGuiManager->SetRenderer(renderer);
 	myImGuiManager->AddTool(std::move(std::make_unique<MeshTool>(renderer)));
 
-	/*{
+	{
 		std::unique_ptr<ModelInstance> pyramid = std::make_unique<ModelInstance>();
-		pyramid->Init(renderer->GetMesh("Pyramid"), "Assets/Textures/Cat.dds");
+		pyramid->Init(modelFactory->GetMesh("Pyramid"), "Assets/Textures/Cat.dds");
 		pyramid->SetScale({ 1,1,1 });
 		pyramid->SetPosition(SimpleUtilities::Vector3f(-8, 2, 3));
 		pyramid->SetName("Pyramid");
-		renderer->AddModelInstance(std::move(pyramid));
+		myModelInstances.push_back(std::move(pyramid));
 	}
 
 	{
 		std::unique_ptr<ModelInstance> cube = std::make_unique<ModelInstance>();
-		cube->Init(myRenderer->GetMesh("Cube"));
+		cube->Init(modelFactory->GetMesh("Cube"));
 		cube->SetScale({ 5,5,5 });
 		cube->SetPosition(SimpleUtilities::Vector3f(-7, 2, 10));
 		cube->SetName("Cube");
-		myRenderer->AddModelInstance(std::move(cube));
+		myModelInstances.push_back(std::move(cube));
 	}
 
 	{
 		std::unique_ptr<ModelInstance> plane = std::make_unique<ModelInstance>();
-		plane->Init(myRenderer->GetMesh("Plane"));
+		plane->Init(modelFactory->GetMesh("Plane"));
 		plane->SetShader("Shaders/WaterReflectionPS.cso", "Shaders/DefaultVS.cso");
 		plane->SetName("Plane");
-		myRenderer->AddModelInstance(std::move(plane));
-	}*/
+		myModelInstances.push_back(std::move(plane));
+	}
 
-	/*{
-		SimpleUtilities::Vector3f position(-10, 0, 30);
-		unsigned int count = 1;
-		for (size_t x = 0; x < 10; ++x)
-		{
-			for (size_t y = 0; y < 10; ++y)
-			{
-				std::unique_ptr<ModelInstance> cube = std::make_unique<ModelInstance>();
-				cube->Init(myRenderer->GetMesh("Cube"));
-				cube->SetPosition(position);
-				cube->SetName("Cube" + std::to_string(count));
-
-				myRenderer->AddModelInstance(std::move(cube));
-
-				++count;
-				position.x += 5;
-			}
-			position.x = -10;
-			position.z += 5;
-		}
-	}*/
-
-	myModelInstances.push_back(std::move(SimpleGlobal::GetModelFactory()->CreateTerrainModel()));
+	myModelInstances.push_back(std::move(modelFactory->CreateTerrainModel()));
+	myModelInstances.push_back(std::move(modelFactory->CreateSkyBoxModel()));
+	myDirectionalLight = std::move(modelFactory->CreateDirectionalLightModel());
 }
 
 void GameWorld::Update()
@@ -86,6 +66,8 @@ void GameWorld::Render()
 	{
 		renderer->Render(*model);
 	}
+
+	renderer->Render(*myDirectionalLight);
 }
 
 void GameWorld::RenderImGui()
