784d6fe225d217bc9a96d27a3788b7ab641893fd
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2023-12-30 02:19:03 +0100

    Bigger scale for plane reflection

--- Source/Engine/Graphics/Model/PlaneReflection.cpp
+++ Source/Engine/Graphics/Model/PlaneReflection.cpp
@@ -6,8 +6,8 @@ PlaneReflection::PlaneReflection()
 	myModelInstance = std::move(SimpleGlobal::GetModelFactory()->CreatePlaneModel());
 	myModelInstance->ClearTextures();
 	myModelInstance->SetName("Reflection");
-	myModelInstance->SetScale({ 5,5,5 });
-	myModelInstance->SetPosition({ 10,-1,-5 });
+	myModelInstance->SetScale({ 25,1,25 });
+	myModelInstance->SetPosition({ 5,-1,5 });
 }
 
 PlaneReflection::~PlaneReflection()
