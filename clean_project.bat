f592ffe2832e28466e1833d43907338a74226542
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-01-01 07:39:31 +0100

    Updated clean_project bat to remove properly and keep necessary stuff for version 8.15

--- clean_project.bat
+++ clean_project.bat
@@ -1,10 +1,9 @@
 @echo off
 
-rem delete all .sln files
 
 del *.sln
 
-rem delete all .vcxproj files
+rmdir /s /q .vs
 
 for /r %%i in (*.vcxproj) do (
     del "%%i" /q
@@ -18,51 +17,64 @@ for /r %%i in (*.vcxproj.user) do (
     del "%%i" /q
 )
 
-rem delete all folder and it's content in Bin except "Assets" and "Shaders" folder
-
-set "folderPath=Bin"
-cd %folderPath%
-for /d %%d in (*) do (
-    if /i not "%%d"=="Assets" if /i not "%%d"=="Shaders" (
-        rmdir /s /q "%%d"
-    )
-)
-
-rem Remove files with .pdb extension
-for %%f in (*.pdb) do (
-    del /q "%%f"
-)
-
-cd ..
-
-
-
-
-rem delete Temp folder
-
 set "folderPaths=Temp"
+
 for %%d in (%folderPaths%) do (
     rd /s /q "%%d"
 )
 
+setlocal enabledelayedexpansion
 
+SET "sourcedir=Bin"
+SET "keepdir=Assets Shaders"
+SET "keepfile=imgui.ini levels.json settings.json SimpleEngine_Simple.exe SimpleEngine_Debug.exe SimpleEngine_Release.exe easy_profiler.dll fmod.dll fmodL.dll fmodstudio.dll fmodstudioL.dll"
 
-
-set "folderPath=Lib"
-set "excludeFile=easy_profiler.lib"
-for %%f in ("%folderPath%\*") do (
-    echo "%%~nxf" | findstr /i /c:"%excludeFile%" >nul && (
-        echo Keeping "%%~nxf"
-    ) || (
-        del /f /q "%%f"
+FOR /d %%a IN ("%sourcedir%\*") DO (
+    set "folderName=%%~nxa"
+    set "keepFolder="
+    for %%k in (%keepdir%) do (
+        if /i "!folderName!"=="%%~k" set "keepFolder=true"
     )
+    if not defined keepFolder RD /S /Q "%%a"
 )
 
+FOR %%a IN ("%sourcedir%\*") DO (
+    set "fileName=%%~nxa"
+    set "keepFile="
+    for %%k in (%keepfile%) do (
+        if /i "!fileName!"=="%%~k" set "keepFile=true"
+    )
+    if not defined keepFile DEL "%%a"
+)
 
+endlocal
 
+cd ..
 
-rem delete hidden ".vs" folder
+setlocal enabledelayedexpansion
 
-rmdir /s /q .vs
+SET "sourcedir=Lib"
+SET "keepdir=Debug"
+SET "keepfile=easy_profiler.dll SoundEngine-FMod.lib SoundEngine-FMod.pdb"
 
-set /p DUMMY=Success cleaning project, press any key to exit
+FOR /d %%a IN ("%sourcedir%\*") DO (
+    set "folderName=%%~nxa"
+    set "keepFolder="
+    for %%k in (%keepdir%) do (
+        if /i "!folderName!"=="%%~k" set "keepFolder=true"
+    )
+    if not defined keepFolder RD /S /Q "%%a"
+)
+
+FOR %%a IN ("%sourcedir%\*") DO (
+    set "fileName=%%~nxa"
+    set "keepFile="
+    for %%k in (%keepfile%) do (
+        if /i "!fileName!"=="%%~k" set "keepFile=true"
+    )
+    if not defined keepFile DEL "%%a"
+)
+
+endlocal
+
+set /p DUMMY=Project cleaned, press any key to exit.
