486288b1b1f9c5ed63936b3ad0270010834e54ac
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-01-02 06:24:52 +0100

    should be enough fuck this wave

--- Source/Engine/Graphics/Shaders/PS/WaterReflectionPS.hlsl
+++ Source/Engine/Graphics/Shaders/PS/WaterReflectionPS.hlsl
@@ -9,28 +9,29 @@ PixelOutput main(PixelInputType aInput)
     PixelOutput output;
     
     float2 normalizedDeviceCoords = aInput.position.xy / float2(resolution.x, resolution.y);
-    float2 uv = normalizedDeviceCoords;
+    
+    float2 reflectUV = normalizedDeviceCoords;
+    float2 refractUV = normalizedDeviceCoords;
     
     float distortionStrength = 0.01f;
     float2 wave = (aDuDvMap.Sample(aSampler, float2(aInput.uv.x + waterMoveFactor, aInput.uv.y)).rg * 2.0f - 1.0f) * distortionStrength;
     float2 wave2 = (aDuDvMap.Sample(aSampler, float2(-aInput.uv.x + waterMoveFactor, aInput.uv.y + waterMoveFactor)).rg * 2.0f - 1.0f) * distortionStrength;
     float2 totalWave = wave + wave2;
     
-    uv += clamp(totalWave, 0.001f, 0.999f);
+    reflectUV += clamp(totalWave, 0.001f, 0.999f);
+    refractUV.y += clamp(totalWave.y, 0.001f, 0.999f);
     
-    float4 reflectColor = aDefaultTexture.Sample(aSampler, uv);
-    float4 refractColor = aRefractionTexture.Sample(aSampler, uv);
-    
-    float4 finalColor = lerp(reflectColor, refractColor, 0.5f);
+    float4 reflectColor = aDefaultTexture.Sample(aSampler, reflectUV);
+    float4 refractColor = aRefractionTexture.Sample(aSampler, refractUV);
     
     float3 toEye = normalize(cameraPosition.xyz - aInput.worldPosition.xyz);
-    float3 fresnel = Fresnel_Schlick(
-        float3(0.75f, 0.75f, 0.75f),
-        float3(0.0f, 1.0f, 0.0f),
-        toEye
-    );
+    float refractiveFactor = dot(toEye, float3(0.0, 1.0, 0.0));
+    refractiveFactor -= 0.1f;
+    refractiveFactor = clamp(refractiveFactor, 0.0f, 1.0f);
     
-    output.color.rgb = fresnel * finalColor.xyz;
+    float4 finalColor = lerp(reflectColor, refractColor, refractiveFactor);
+    
+    output.color.rgb =  finalColor.xyz;
     output.color.a = 1.0f;
 
     return output;
