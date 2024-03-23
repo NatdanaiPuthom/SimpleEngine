136947085bbe3cc6db5efebefe60d84386d0835d
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-03-23 23:41:24 +0100

    Added ifdef for editor

--- SimpleEngine/Source/Editor/Editor.cpp
+++ SimpleEngine/Source/Editor/Editor.cpp
@@ -1,10 +1,14 @@
 #include "Editor/Precomplied/EditorPch.hpp"
-#include "Graphics/Renderer/Renderer.hpp"
 #include "Editor/Editor.hpp"
+
+#ifndef _SIMPLE
+#include "Graphics/Renderer/Renderer.hpp"
 #include "Editor/Menu/MainMenuBar.hpp"
+#endif
 
 namespace Simple
 {
+#ifndef _SIMPLE
 	Editor::Editor()
 	{
 		AddTool(std::move(std::make_unique<Tool::MainMenuBar>()));
@@ -49,4 +53,15 @@ namespace Simple
 	{
 		myTools.push_back(std::move(aTool));
 	}
+
+#else
+	Editor::Editor() {}
+	Editor::~Editor() {}
+
+	void Editor::Init() {}
+	void Editor::Update() {}
+	void Editor::Render() {}
+
+	void Editor::AddTool(std::unique_ptr<Simple::ToolInterface> /*aTool*/) {}
+#endif
 }
