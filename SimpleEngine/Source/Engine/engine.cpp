bce84897e42d4767ef940eec19878f874cf11941
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-03-29 18:09:04 +0100

    v9.27.6

--- SimpleEngine/Source/Engine/engine.cpp
+++ SimpleEngine/Source/Engine/engine.cpp
@@ -154,7 +154,7 @@ namespace Simple
 		std::unique_ptr<HWND> hwnd = std::make_unique<HWND>();
 		*hwnd = CreateWindow(
 			L"Natdanai",
-			L"SimpleEngine v9.26.6",
+			L"SimpleEngine v9.27.6",
 			WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
 			CW_USEDEFAULT,
 			CW_USEDEFAULT,
