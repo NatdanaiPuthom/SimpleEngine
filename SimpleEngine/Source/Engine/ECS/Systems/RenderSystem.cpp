18c74ecdacc65d86fbcdae6c74babf819a7fb515
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-05-15 18:44:21 +0200

    preparing for implement proper directional light and shadow

--- SimpleEngine/Source/Engine/ECS/Systems/RenderSystem.cpp
+++ SimpleEngine/Source/Engine/ECS/Systems/RenderSystem.cpp
@@ -17,26 +17,62 @@ namespace ECS
 
 	void RenderSystem::Init()
 	{
-		ECS::Entity entity = myEntityManager->CreateEntity();
-		entity->AddComponent<TransformComponent>();
-		entity->AddComponent<MeshComponent>();
-
-		entity->SetName("SkyBox");
-
 		Graphics::GraphicsEngine* graphicsEngine = Global::GetGraphicsEngine();
-		entity->GetComponent<MeshComponent>()->shader = graphicsEngine->GetShader(Graphics::eShaderType::SkyBox).get();
-		entity->GetComponent<MeshComponent>()->texture = graphicsEngine->GetTexture(Graphics::eTextureType::SkyBox_DayCloud).get();
-		entity->GetComponent<MeshComponent>()->mesh = graphicsEngine->GetModelFactory()->GetPrimitiveShape(Graphics::ePrimitiveShape::SkyBox);
+
+		{
+			ECS::Entity entity = myEntityManager->CreateEntity();
+			entity->SetName("SkyBox");
+
+			entity->AddComponent<TransformComponent>();
+			entity->AddComponent<MeshComponent>();
+
+			entity->GetComponent<MeshComponent>()->shader = graphicsEngine->GetShader(Graphics::eShaderType::SkyBox).get();
+			entity->GetComponent<MeshComponent>()->texture = graphicsEngine->GetTexture(Graphics::eTextureType::SkyBox_DayCloud).get();
+			entity->GetComponent<MeshComponent>()->mesh = graphicsEngine->GetModelFactory()->GetPrimitiveShape(Graphics::ePrimitiveShape::SkyBox);
+		}
+
+		{
+			ECS::Entity floor = myEntityManager->CreateEntity();
+			floor->SetName("Floor");
+
+			floor->AddComponent<TransformComponent>();
+			floor->AddComponent<MeshComponent>();
+
+			floor->GetComponent<MeshComponent>()->shader = graphicsEngine->GetShader(Graphics::eShaderType::Unlit_Default).get();
+			floor->GetComponent<MeshComponent>()->texture = graphicsEngine->GetTexture(Graphics::eTextureType::Default).get();
+			floor->GetComponent<MeshComponent>()->mesh = graphicsEngine->GetModelFactory()->GetPrimitiveShape(Graphics::ePrimitiveShape::Cube);
+			floor->GetComponent<TransformComponent>()->transform.SetPosition({ 0.0f, -2.0f, 0.0f });
+			floor->GetComponent<TransformComponent>()->transform.SetScale({ 50.0f, 1.0f, 50.0f });
+		}
 
 
-		ECS::Entity test = myEntityManager->CreateEntity();
-		test->AddComponent<TransformComponent>();
-		test->AddComponent<MeshComponent>();
-		test->SetName("Test");
+		for (size_t i = 0; i < 10; ++i)
+		{
+			ECS::Entity e = myEntityManager->CreateEntity();
+			e->SetName("Horizontal Cube " + std::to_string(i));
 
-		test->GetComponent<MeshComponent>()->shader = graphicsEngine->GetShader(Graphics::eShaderType::PBR_Default).get();
-		test->GetComponent<MeshComponent>()->texture = graphicsEngine->GetTexture(Graphics::eTextureType::Default).get();
-		test->GetComponent<MeshComponent>()->mesh = graphicsEngine->GetModelFactory()->GetPrimitiveShape(Graphics::ePrimitiveShape::Cube);
+			e->AddComponent<TransformComponent>();
+			e->AddComponent<MeshComponent>();
+
+			e->GetComponent<MeshComponent>()->shader = graphicsEngine->GetShader(Graphics::eShaderType::PBR_Default).get();
+			e->GetComponent<MeshComponent>()->texture = graphicsEngine->GetTexture(Graphics::eTextureType::Default).get();
+			e->GetComponent<MeshComponent>()->mesh = graphicsEngine->GetModelFactory()->GetPrimitiveShape(Graphics::ePrimitiveShape::Cube);
+			e->GetComponent<TransformComponent>()->transform.SetPosition({ 0.0f + i * 3.0f, 0.0f, 0.0f });
+		}
+
+		for (size_t i = 0; i < 10; ++i)
+		{
+			ECS::Entity e = myEntityManager->CreateEntity();
+			e->SetName("Vertical Cube " + std::to_string(i));
+
+			e->AddComponent<TransformComponent>();
+			e->AddComponent<MeshComponent>();
+
+			e->GetComponent<MeshComponent>()->shader = graphicsEngine->GetShader(Graphics::eShaderType::PBR_Default).get();
+			e->GetComponent<MeshComponent>()->texture = graphicsEngine->GetTexture(Graphics::eTextureType::Default).get();
+			e->GetComponent<MeshComponent>()->mesh = graphicsEngine->GetModelFactory()->GetPrimitiveShape(Graphics::ePrimitiveShape::Cube);
+			e->GetComponent<TransformComponent>()->transform.SetPosition({ 15.0f, 0.0f, 0.0f + i * 3.0f });
+		}
 	}
 
 	void RenderSystem::Update()
