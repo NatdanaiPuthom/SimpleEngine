1017721980d52e26e09129b131a3e8024ced619e
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-06-03 21:22:54 +0200

    Added support for multiple pointlights

--- SimpleEngine/Source/Shader/PixelShader/PointLightCullPS.hlsl
+++ SimpleEngine/Source/Shader/PixelShader/PointLightCullPS.hlsl
@@ -3,39 +3,33 @@
 PixelOutput main(PixelInputType aInput)
 {
     PixelOutput output;
-    
-    if (currentPointLightCount == 0)
-    {
-        discard;
-        output.color = float4(0.0f, 0.0f, 0.0f, 0.0f);
-        return output;
-    }
    
-    float2 uv = aInput.position.xy / resolution.xy;
+    const float2 uv = aInput.position.xy / resolution.xy;
    
-    float3 position = GlobalBufferPositionTexture.Sample(GlobalDefaultSampler, uv).rgb;
-    float3 albedo = GlobalBufferAlbedoTexture.Sample(GlobalDefaultSampler, uv).rgb;
-    float3 pixelNormal = normalize(2.0f * GlobalBufferNormalTexture.Sample(GlobalDefaultSampler, uv).xyz - 1.0f);
-    float3 material = GlobalBufferMaterialTexture.Sample(GlobalDefaultSampler, uv).rgb;
+    const float3 position = GlobalBufferPositionTexture.Sample(GlobalDefaultSampler, uv).rgb;
+    const float3 albedo = GlobalBufferAlbedoTexture.Sample(GlobalDefaultSampler, uv).rgb;
+    const float3 pixelNormal = normalize(2.0f * GlobalBufferNormalTexture.Sample(GlobalDefaultSampler, uv).xyz - 1.0f);
+    const float3 material = GlobalBufferMaterialTexture.Sample(GlobalDefaultSampler, uv).rgb;
     
-    float3 toEye = normalize(cameraPosition.xyz - position.xyz);
+    const float3 toEye = normalize(cameraPosition.xyz - position.xyz);
     
-    float metalness = material.r;
-    float roughness = material.g;
+    const float metalness = material.r;
+    const float roughness = material.g;
     
-    float3 specularColor = lerp((float3) 0.04f, albedo.rgb, metalness);
-    float3 diffuseColor = lerp((float3) 0.00f, albedo.rgb, 1 - metalness); 
+    const float3 specularColor = lerp((float3) 0.04f, albedo.rgb, metalness);
+    const float3 diffuseColor = lerp((float3) 0.00f, albedo.rgb, 1 - metalness);
     
     float3 accumulatedPointLight = 0;
-    uint pointLightIndex = currentPointLightCount - 1;
-   
-    if (distance(pointLights[pointLightIndex].position.xyz, position.xyz) <= pointLights[pointLightIndex].range)
+    for (unsigned int i = 0; i < currentPointLightCount; i++)
     {
-        accumulatedPointLight += EvaluatePointLight(
+        if (distance(pointLights[i].position.xyz, position.xyz) <= pointLights[i].range)
+        {
+            accumulatedPointLight += EvaluatePointLight(
 			    diffuseColor, specularColor, pixelNormal, roughness,
-			    pointLights[pointLightIndex].color.rgb, pointLights[pointLightIndex].color.w, pointLights[pointLightIndex].range, pointLights[pointLightIndex].position.xyz,
+			    pointLights[i].color.rgb, pointLights[i].color.w, pointLights[i].range, pointLights[i].position.xyz,
 			    toEye.xyz, position.xyz);
-    } 
+        }
+    }
     
     output.color = float4(accumulatedPointLight, 1.0f);
     return output;
