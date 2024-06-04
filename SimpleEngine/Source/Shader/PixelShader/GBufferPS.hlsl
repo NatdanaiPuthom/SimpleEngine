6209f6b598099d64751f2585d1e0e5cfb4d9f780
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-06-04 22:21:42 +0200

    Fixed wrongly added normal, tangent, bitangent calculations

--- SimpleEngine/Source/Shader/PixelShader/GBufferPS.hlsl
+++ SimpleEngine/Source/Shader/PixelShader/GBufferPS.hlsl
@@ -2,15 +2,14 @@
 
 GBufferOutput main(PixelInputType aInput)
 {
-    float2 uv = aInput.uv;
+    const float2 uv = aInput.uv;
     
-    float4 diffuse = GlobalAlbedo.Sample(GlobalDefaultSampler, uv) * aInput.color; //NOTE(v9.37.0): Why float4 when we will never use alpha-channel
-    float3 material = GlobalMaterialTexture.Sample(GlobalDefaultSampler, uv).rgb;
-    float3 normal = GlobalNormalTexture.Sample(GlobalDefaultSampler, uv).wyz; //NOTE(v9.37.0): Why are we using wyz?
+    const float4 diffuse = GlobalAlbedo.Sample(GlobalDefaultSampler, uv) * aInput.color; //NOTE(v9.37.0): Why float4 when we will never use alpha-channel
+    const float3 material = GlobalMaterialTexture.Sample(GlobalDefaultSampler, uv).rgb;
+    float3 normal = GlobalNormalTexture.Sample(GlobalDefaultSampler, uv).wyz;
     
-    float3 ambientLightColorAndIntensity = float3(1.0f, 1.0f, 1.0f); //NOTE(v9.37.0):  No clue, just put 1.0f for now
-    float3 ambientColor = diffuse.xyz * ambientLightColorAndIntensity;
-    float3 directColor = float3(0.0f, 0.0f, 0.0f); //NOTE(v9.37.0):  What is purpose of this?
+    const float3 ambientLightColorAndIntensity = float3(1.0f, 1.0f, 1.0f); //NOTE(v9.37.0):  No clue, just put 1.0f for now
+    const float3 ambientColor = diffuse.xyz * ambientLightColorAndIntensity;
     
     normal = 2.0f * normal - 1.0f; //NOTE(v9.37.0): Unpacking normal
     normal.z = sqrt(1.0f - saturate(normal.x * normal.x + normal.y * normal.y)); //NOTE(v9.37.0): No clue what this does
@@ -23,12 +22,12 @@ GBufferOutput main(PixelInputType aInput)
     );
     
     TBN = transpose(TBN); //NOTE(v9.37.0): Why transpose? NOTE(v10.0.1): Inverse of TBN matrix is tranpose version of it and transpose is faster than inverse
-    float3 pixelNormal = normalize(mul(TBN, normal)); //NOTE(v9.37.0): What is PixelNormal?
+    const float3 pixelNormal = normalize(mul(TBN, normal)); //NOTE(v9.37.0): What is PixelNormal?
     
     GBufferOutput output;
     
     output.position = aInput.worldPosition;
-    output.albedo = float4(ambientColor + directColor, diffuse.w);;
+    output.albedo = float4(ambientColor, diffuse.w);;
     output.material = float4(material, diffuse.w);;
     output.normal = float4(0.5f + 0.5f * pixelNormal, 1.f); //NOTE(v9.37.0): Packing normal to between 0 and 1
     output.ambientOcclusionAndCustom.r = GlobalNormalTexture.Sample(GlobalDefaultSampler, uv).z;
--- SimpleEngine/Source/Shader/VertexShader/SkyBox.hlsl
+++ SimpleEngine/Source/Shader/VertexShader/SkyBox.hlsl
@@ -4,22 +4,17 @@ PixelInputType main(VertexInputType aInput)
 {
     PixelInputType output;
     
-    float4 vertexObjectPos = aInput.position;
-    float4 vertexWorldPos = mul(modelWorld, vertexObjectPos);
-    float4 vertexClipPos = mul(worldToClipMatrix, vertexWorldPos);
+    const float4 vertexObjectPos = aInput.position;
+    const float4 vertexWorldPos = mul(modelWorld, vertexObjectPos);
+    const float4 vertexClipPos = mul(worldToClipMatrix, vertexWorldPos);
     
     output.position = vertexClipPos;
     output.worldPosition = vertexWorldPos.xyzw;
     output.uv = float2(aInput.uv.x, 1.0 - aInput.uv.y);
     
-    //output.normal = aInput.normal;
-    //output.tangent = aInput.tangent;
-    //output.bitangent = aInput.bitangent;
-    
-    //TO-DO(v10.0.3): Check if this is correct
-    output.normal = mul(float4(aInput.normal, 1.0f), modelWorld).xyz;
-    output.tangent = mul(float4(aInput.tangent, 1.0f), modelWorld).xyz;
-    output.bitangent = mul(float4(aInput.bitangent, 1.0f), modelWorld).xyz;
+    output.normal = mul((float3x3) modelWorld, aInput.normal);
+    output.tangent = mul((float3x3) modelWorld, aInput.tangent);
+    output.bitangent = mul((float3x3) modelWorld, aInput.bitangent);
     
     output.clip = aInput.clip;
     output.color = aInput.color;
