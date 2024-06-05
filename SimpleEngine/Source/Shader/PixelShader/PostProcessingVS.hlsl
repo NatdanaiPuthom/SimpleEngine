746000c017672998f5860fc12f16a61ab37343f5
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-06-06 00:47:28 +0200

    turned off postprocessing for now

--- SimpleEngine/Source/Shader/PixelShader/PostProcessingVS.hlsl
+++ SimpleEngine/Source/Shader/PixelShader/PostProcessingVS.hlsl
@@ -8,15 +8,15 @@ PixelOutput main(FullScreenVertexToPixel aInput)
     
     float3 albedo = GlobalBufferAlbedoTexture.Sample(GlobalDefaultSampler, uv).rgb;
       
-    const float luminance = dot(float3(0.2126f, 0.7152f, 0.0722f), albedo);
-    const float3 newSaturationColor = luminance + saturation * (albedo - luminance);
-    albedo = newSaturationColor;
+    //const float luminance = dot(float3(0.2126f, 0.7152f, 0.0722f), albedo);
+    //const float3 newSaturationColor = luminance + saturation * (albedo - luminance);
+    //albedo = newSaturationColor;
     
-    const float3 newExposureColor = exp2(exposure) * albedo;
-    albedo = newExposureColor;
+    //const float3 newExposureColor = exp2(exposure) * albedo;
+    //albedo = newExposureColor;
     
-    const float3 newContrast = 0.18f * pow(albedo / 0.18f, constrast);
-    albedo = newContrast;
+    //const float3 newContrast = 0.18f * pow(albedo / 0.18f, constrast);
+    //albedo = newContrast;
     
     output.color = float4(albedo, 1.0f);
     return output;
