33900f3f28e3496cebf35e7797353046057aea65
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-06-06 17:13:30 +0200

    updated postprocess editor window

--- SimpleEngine/Source/Editor/Windows/PostProcessingWindow.cpp
+++ SimpleEngine/Source/Editor/Windows/PostProcessingWindow.cpp
@@ -20,7 +20,7 @@ namespace Editor
 		Graphics::GraphicsEngine* graphicsEngine = Global::GetGraphicsEngine();
 		const PostProcessData& postProcessData = graphicsEngine->GetPostProcessData();
 
-		if (ImGui::Begin("PostProcess"))
+		if (ImGui::Begin("PostProcess", 0, ImGuiWindowFlags_NoMove))
 		{
 			Math::Vector3f tintValue = postProcessData.tint;
 			if (ImGui::ColorPicker3("Tint", &tintValue.x))
@@ -51,6 +51,27 @@ namespace Editor
 			{
 				graphicsEngine->SetBlackPoint(blackpoint);
 			}
+		}
+
+		ImGui::End();
+
+		if (ImGui::Begin("PostProcessImage", 0, ImGuiWindowFlags_NoMove))
+		{
+			const ImVec2 windowSize = ImGui::GetWindowSize();
+
+			const float aspectRatio = 16.0f / 9.0f;
+
+			float newWidth = windowSize.x;
+			float newHeight = newWidth / aspectRatio;
+
+			if (newHeight > windowSize.y)
+			{
+				newHeight = windowSize.y;
+				newWidth = newHeight * aspectRatio;
+			}
+
+			ImTextureID texture = graphicsEngine->GetShaderResourceView(Graphics::eRenderTargetType::PostProcessing, 0).Get();
+			ImGui::Image(texture, ImVec2(newWidth, newHeight));
 		}
 
 		ImGui::End();
