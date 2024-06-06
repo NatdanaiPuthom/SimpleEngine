c967dfbad1989f5f384affd2bb29ff525a1a92b5
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-06-06 18:20:32 +0200

    v10.0.5

--- SimpleEngine/Source/Engine/engine.cpp
+++ SimpleEngine/Source/Engine/engine.cpp
@@ -154,7 +154,7 @@ namespace Simple
 
 		return CreateWindow(
 			L"Natdanai",
-			L"SimpleEngine v10.0.4 (Simple, it's just that easy)",
+			L"SimpleEngine v10.0.5 (Simple, it's just that easy)",
 			WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
 			CW_USEDEFAULT,
 			CW_USEDEFAULT,
