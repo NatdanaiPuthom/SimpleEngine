49936643cc4ffc99fd789813aa10e1d7ebb8ad71
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-07-01 06:30:08 +0200

    Removed unnessesary include from editor precomplie header

--- SimpleEngine/Source/Editor/Precomplied/EditorPch.hpp
+++ SimpleEngine/Source/Editor/Precomplied/EditorPch.hpp
@@ -37,18 +37,10 @@
 #include "Engine/Math/Matrix4x4.hpp"
 #include "Engine/Math/Transform.hpp"
 #include "Engine/Math/Math.hpp"
-#include "Engine/Math/Intersection.hpp"
 
-#include "Engine/SimpleUtilities/HashStuff.hpp"
-#include "Engine/SimpleUtilities/Ray.hpp"
-#include "Engine/SimpleUtilities/Plane.hpp"
-#include "Engine/SimpleUtilities/AABB2D.hpp"
-#include "Engine/SimpleUtilities/AABB3D.hpp"
-#include "Engine/SimpleUtilities/Bounds.hpp"
 #include "Engine/SimpleUtilities/Utility.hpp"
 
 #include "Engine/Global.hpp"
-#include "Game/World.hpp"
 
 #pragma message("---------Never give up on your dreams!---------------")
 
