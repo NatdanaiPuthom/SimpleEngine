2fb05a4cdfe775d0e3810677750e4ce1ad4e12b0
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-07-08 02:32:02 +0200

    Adjusted to AND operator

--- SimpleEngine/Source/Editor/Menu/MainMenuBar.cpp
+++ SimpleEngine/Source/Editor/Menu/MainMenuBar.cpp
@@ -116,15 +116,16 @@ namespace Editor
 			if (ImGui::Begin("Scene", 0, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_AlwaysAutoResize))
 			{
 				Graphics::GraphicsEngine* graphicsEngine = Global::GetGraphicsEngine();
+				const Graphics::eRasterizerState currentRasterizerState = graphicsEngine->GetCurrentRasterizerState();
+
 				ImTextureID textureID = graphicsEngine->GetShaderResourceView(Graphics::eRenderTargetType::PostProcessing).Get();
 
-				const Graphics::eRasterizerState currentRasterizerState =  graphicsEngine->GetCurrentRasterizerState();
 				if (currentRasterizerState != Graphics::eRasterizerState::BackfaceCulling
-					|| currentRasterizerState != Graphics::eRasterizerState::NoFaceCulling)
+					&& currentRasterizerState != Graphics::eRasterizerState::NoFaceCulling)
 				{
 					textureID = graphicsEngine->GetShaderResourceView(Graphics::eRenderTargetType::Deferred).Get();
 				}
-					
+
 				ImVec2 size = ImGui::GetContentRegionAvail();
 				ImGui::Image(textureID, size);
 			}
