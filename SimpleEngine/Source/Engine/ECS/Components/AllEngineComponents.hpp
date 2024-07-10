309cc6967b47f72ac82bf1b6813d77c47c6c1fcb
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-07-10 22:25:10 +0200

    Moved nullcomponent out of Core

--- SimpleEngine/Source/Engine/ECS/Components/AllEngineComponents.hpp
+++ SimpleEngine/Source/Engine/ECS/Components/AllEngineComponents.hpp
@@ -1,9 +1,10 @@
 #pragma once
+#include "Engine/ECS/Components/NullComponent.hpp"
+
 #include "Engine/ECS/Components/Core/TransformComponent.hpp"
 #include "Engine/ECS/Components/Core/MeshComponent.hpp"
 #include "Engine/ECS/Components/Core/AnimatedComponent.hpp"
 #include "Engine/ECS/Components/Core/AnimtionPlayerComponent.hpp"
 #include "Engine/ECS/Components/Core/SkyBoxComponent.hpp"
 #include "Engine/ECS/Components/Core/DirectionalLightComponent.hpp"
-#include "Engine/ECS/Components/Core/NullComponent.hpp"
 #include "Engine/ECS/Components/Core/ScriptComponent.hpp"
