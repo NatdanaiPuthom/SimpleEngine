a744f67f6375101a8af5e652461417777b926d6b
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-07-13 00:34:03 +0200

    Update comments

--- SimpleEngine/Source/Game/Systems/Example/ExampleSystem.hpp
+++ SimpleEngine/Source/Game/Systems/Example/ExampleSystem.hpp
@@ -13,27 +13,27 @@ namespace ECS
 	class ExampleSystem final : public ECS::System
 	{
 	public:
-		/*
+		/* NOTE (v11.3.0)
 		* System-constructor require to pass EntityComponentSystem pointer but you can also add more parameters to this class itself
 		* Example: ExampleSystem(EntityComponentSystem* aEntityComponentSystem, int aValue);
 		*/
 
 		ExampleSystem(EntityComponentSystem* aEntityComponentSystem);
 
-		/*
+		/* NOTE (v11.3.0)
 		* These functions below are pure-virtual which is required for every System
 		*/
 
-		void Update() override;
 		virtual std::unique_ptr<System> Clone(EntityComponentSystem* aEntityComponentSystem) const override;
 
-		/*
+		/* NOTE (v11.3.0)
 		* These virtual functions below doesn't have to be implemented if not needed
 		*/
 
 		~ExampleSystem() override;
 
 		void Init() override;
+		void Update() override;
 		void Render() override;
 
 		void EarlyUpdate() override;
