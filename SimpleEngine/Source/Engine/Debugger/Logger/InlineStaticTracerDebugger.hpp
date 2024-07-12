31a528d6e8d22dbdc7c3e1da723d033fd957b4b4
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-07-13 00:27:08 +0200

    Added macro for easier testing and comments

--- SimpleEngine/Source/Engine/Debugger/Logger/InlineStaticTracerDebugger.hpp
+++ SimpleEngine/Source/Engine/Debugger/Logger/InlineStaticTracerDebugger.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "Engine/SimpleUtilities/MacroUtility.hpp"
 #include <Windows.h>
 #include <string>
 
@@ -9,16 +10,25 @@ namespace SimpleTracker
 		InlineLogger(const std::string& aString)
 		{
 			OutputDebugStringA("\n");
-			OutputDebugStringA("===========================================================");
+			OutputDebugStringA("=========================================================== ");
 			OutputDebugStringA(aString.c_str());
-			OutputDebugStringA("===========================================================\n");
+			OutputDebugStringA(" ===========================================================\n");
 		}
 	};
 }
 
-//#include <vector>
-//inline static std::vector<int> example = []() -> std::vector<int>
-//	{
-//		SimpleTracker::InlineLogger logger("example");
-//		return {};
-//	}();
+#define INLINELOGGER(TYPE, NAME, RETURN) inline static TYPE NAME = []() -> TYPE {SimpleTracker::InlineLogger logger(CONVERT_TO_STRING(NAME)); return RETURN; }();
+
+/*
+* #include <vector>
+*
+* Example 1
+* INLINELOGGER(std::vector<int>, example1, std::vector<int>());
+
+* Example 2
+* inline static std::vector<int> example2 = []() -> std::vector<int>
+* 	{
+* 		SimpleTracker::InlineLogger logger("example2");
+* 		return std::vector<int>();
+* 	}();
+*/
