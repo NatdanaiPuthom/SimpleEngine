373c6f565dc69eb6c89c0b6e8c96fde153b8c8b8
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-07-15 11:45:26 +0200

    Hopefully this will print file more consistence as sometime it does not print

--- SimpleEngine/Source/Engine/NoClueWhatToName/EasyProfilerOutput.hpp
+++ SimpleEngine/Source/Engine/NoClueWhatToName/EasyProfilerOutput.hpp
@@ -1,36 +1,28 @@
 #pragma once
-#include <fstream>
-#include <sstream>
-#include <External/profiler.h>
 #include "Engine/SimpleUtilities/Utility.hpp"
+#include "External/profiler.h"
 
 namespace Simple
 {
 	void EasyProfilerOutput()
 	{
-		const std::string baseFileName = SimpleUtilities::GetAbsolutePath(SIMPLE_FILENAME_PROFILER);
-		const size_t dotPosition = baseFileName.find_last_of('.');
-		const std::string extension = baseFileName.substr(dotPosition);
+		std::string filePath = SimpleUtilities::GetAbsolutePath(SIMPLE_FILENAME_PROFILER);
+		filePath;
+
+		const size_t dotPosition = filePath.find_last_of('.');
+		const std::string extension = filePath.substr(dotPosition);
 
 #ifdef _DEBUG
-		const std::string configuration = "_Debug(";
+		filePath = filePath.substr(0, dotPosition) + "_Debug";
 #elif _RELEASE
-		const std::string configuration = "_Release(";
+		filePath = filePath.substr(0, dotPosition) + "_Release";
 #elif _SIMPLE
-		const std::string configuration = "_Simple(";
+		filePath = filePath.substr(0, dotPosition) + "_Simple";
 #endif
+		filePath += extension;
 
-		std::string fileName = baseFileName;
-		unsigned int counter = 1;
+		const std::string fileNameWithCounter = SimpleUtilities::AppendCounterIfAlreadyExist(filePath);
 
-		while (std::ifstream(fileName.c_str()))
-		{
-			std::ostringstream oss;
-			oss << baseFileName.substr(0, dotPosition) << configuration << counter << ")" << extension;
-			fileName = oss.str();
-			counter++;
-		}
-
-		PROFILER_DUMP_FILE(fileName.c_str());
+		PROFILER_DUMP_FILE(fileNameWithCounter.c_str());
 	}
 }
