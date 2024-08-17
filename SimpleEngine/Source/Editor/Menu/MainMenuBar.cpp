a9a3eb2b529634279127b54f68147e9f9b484d87
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2024-08-17 03:12:03 +0200

    Gizmos can now adjust scale

--- SimpleEngine/Source/Editor/Menu/MainMenuBar.cpp
+++ SimpleEngine/Source/Editor/Menu/MainMenuBar.cpp
@@ -158,6 +158,19 @@ namespace Editor
 
 						static ImGuizmo::OPERATION operation = ImGuizmo::OPERATION::TRANSLATE;
 
+						if (MainSingleton::GetInputManager().IsKeyPressed('T'))
+						{
+							operation = ImGuizmo::OPERATION::TRANSLATE;
+						}
+						else if (MainSingleton::GetInputManager().IsKeyPressed('R'))
+						{
+							operation = ImGuizmo::OPERATION::ROTATE;
+						}
+						else if (MainSingleton::GetInputManager().IsKeyPressed('S'))
+						{
+							operation = ImGuizmo::OPERATION::SCALE;
+						}
+
 						if (ImGuizmo::Manipulate(&view(1, 1),
 							&proj(1, 1),
 							operation,
@@ -165,7 +178,20 @@ namespace Editor
 							&objectMatrix(1, 1)
 						))
 						{
-							transformComponent->transform.SetPosition(objectMatrix.GetPosition());
+							switch (operation)
+							{
+							case ImGuizmo::OPERATION::TRANSLATE:
+								transformComponent->transform.SetPosition(objectMatrix.GetPosition());
+								break;
+							case ImGuizmo::OPERATION::ROTATE:
+								//TO-DO(v11.4.1): Fix Quaternion and Rotation pls!!! It has been forever!
+								break;
+							case ImGuizmo::OPERATION::SCALE:
+								transformComponent->transform.SetScale(objectMatrix.GetScale());
+								break;
+							default:
+								break;
+							}
 						}
 					}
 				}
