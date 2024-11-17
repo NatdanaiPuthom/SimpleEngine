051d6aaf4d6da034452cf139be8afacc67413db8
EmilOlofsson <b.emil.olofsson@gmail.com>
2024-11-17 15:19:13 +0100

    Added color class

---
+++ SimpleEngine/Source/Engine/SimpleUtilities/Color.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace SimpleUtilities
+{
+
+	class Color final
+	{
+	public:
+
+		constexpr Color() = default;
+		constexpr Color(const float aR, const float aG, const float aB, const float aA)
+			: r(aR)
+			, g(aG)
+			, b(aB)
+			, a(aA)
+		{
+		}
+
+		float r = 0.f;
+		float g = 0.f;
+		float b = 0.f;
+		float a = 1.f;
+
+
+	};
+
+	namespace Colors
+	{
+		inline static constexpr Color Black = Color(0.f, 0.f, 0.f, 1.f);
+		inline static constexpr Color Red = Color(1.f, 0.f, 0.f, 1.f);
+		inline static constexpr Color Green = Color(0.f, 1.f, 0.f, 1.f);
+		inline static constexpr Color Blue = Color(0.f, 0.f, 1.f, 1.f);
+		inline static constexpr Color Yellow = Color(1.f, 1.f, 0.f, 1.f);
+		inline static constexpr Color White = Color(1.f, 1.f, 1.f, 1.f);
+	}
+}
