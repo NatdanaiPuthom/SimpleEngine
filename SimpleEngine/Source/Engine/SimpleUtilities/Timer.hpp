24bfc9fb6759f852ec07661fc40fbb91123b62a8
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-01-28 02:52:43 +0100

    Added Stopwatch to Timer class

--- SimpleEngine/Source/Engine/SimpleUtilities/Timer.hpp
+++ SimpleEngine/Source/Engine/SimpleUtilities/Timer.hpp
@@ -13,29 +13,38 @@ namespace SimpleUtilities
 		void Update();
 		void Reset();
 
+		void StartTimer();
+		double EndTimer();
+
 		float GetDeltaTime() const;
 		float GetTotalMinutes() const;
 		float GetTotalHours() const;
 		double GetTotalTime() const;
 	private:
+		void ResetTimer();
+	private:
+		std::chrono::high_resolution_clock::time_point myStartTimerTime;
+		std::chrono::high_resolution_clock::time_point myEndTimerTime;
 		std::chrono::high_resolution_clock::time_point myStartTime;
 		const double myCapDeltaTime;
 		double myTotalTime;
 		float myDeltaTime;
+		bool myTimerIsRunning;
 	};
 
-	Timer::Timer()
+	inline Timer::Timer()
 		: myStartTime(std::chrono::high_resolution_clock::now())
 		, myCapDeltaTime(1.0 / 60)
 		, myTotalTime(0.0)
 		, myDeltaTime(0.0f)
+		, myTimerIsRunning(false)
 
 	{
 	}
 
 #undef min //NOTE(v10.0.0): Where did this come from?
 
-	void Timer::Update()
+	inline void Timer::Update()
 	{
 		const std::chrono::duration<double> deltaTime = std::chrono::high_resolution_clock::now() - myStartTime;
 
@@ -45,14 +54,37 @@ namespace SimpleUtilities
 		myStartTime = std::chrono::high_resolution_clock::now();
 	}
 
-	void Timer::Reset()
+	inline void Timer::Reset()
 	{
 		myDeltaTime = 0.0f;
 		myTotalTime = 0.0;
 		myStartTime = std::chrono::high_resolution_clock::now();
 	}
 
-	float Timer::GetDeltaTime() const
+	inline void Timer::StartTimer()
+	{
+		myStartTimerTime = std::chrono::high_resolution_clock::now();
+		myTimerIsRunning = true;
+	}
+
+	inline double Timer::EndTimer()
+	{
+		if (myTimerIsRunning)
+		{
+			myEndTimerTime = std::chrono::high_resolution_clock::now();
+			const double time = std::chrono::duration<double>(myEndTimerTime - myStartTimerTime).count();
+
+			ResetTimer();
+
+			return time;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	inline float Timer::GetDeltaTime() const
 	{
 		return myDeltaTime;
 	}
@@ -67,8 +99,15 @@ namespace SimpleUtilities
 		return (GetTotalMinutes() / 60.0f);
 	}
 
-	double Timer::GetTotalTime() const
+	inline double Timer::GetTotalTime() const
 	{
 		return myTotalTime;
+	}
+
+	inline void Timer::ResetTimer()
+	{
+		myStartTimerTime = std::chrono::high_resolution_clock::time_point{};
+		myEndTimerTime = std::chrono::high_resolution_clock::time_point{};
+		myTimerIsRunning = false;
 	}
 }
