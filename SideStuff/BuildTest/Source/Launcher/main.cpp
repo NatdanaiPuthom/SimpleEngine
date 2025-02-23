13304826819667cddad3e8534a38d2623e597953
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-02-23 18:26:43 +0100

    Added empty project

---
+++ SideStuff/BuildTest/Source/Launcher/main.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+using namespace Simple;
+
+int __stdcall wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int nCmdShow)
+{
+	return 0;
+}
---
+++ SideStuff/BuildTest/premake5.lua
@@ -0,0 +1,88 @@
+
+directories = {}
+directories["Root"]					= os.realpath("\\")
+directories["Bin"]					= os.realpath("Bin\\")
+directories["Local"]				= os.realpath("Local\\")
+directories["Source"]				= os.realpath("Source\\")
+directories["Temp"]					= os.realpath("Temp\\")
+directories["Dependencies"]			= os.realpath("Dependencies\\")
+
+directories["Lib"]					= directories.Dependencies .. "Lib\\"
+directories["Launcher"]				= directories.Source .. "Launcher\\"
+
+for key, path in pairs(directories) do
+    if key ~= "Root" then 
+        os.mkdir(directories[key])
+		print(directories[key])
+    end
+end
+
+
+workspace "SimpleEngine"
+	startproject "Launcher" 
+	architecture "x64"
+	language "C++"
+	cdialect "C17"
+	cppdialect "C++20"
+	warnings "Extra"
+	toolset "msc-v143"
+	objdir (directories.Temp)
+
+	configurations {
+		"Debug",
+		"Release",
+		"Simple"
+	}
+
+	filter "configurations:Debug" 
+		defines { "_DEBUG" }
+		symbols "On"
+		optimize "Off"
+		runtime "Debug"
+		linktimeoptimization "Off"
+
+	filter "configurations:Release"
+		defines { "_RELEASE" }
+		symbols "On"
+		optimize "Speed"
+		runtime "Release"
+		linktimeoptimization "On"
+
+	filter "configurations:Simple"
+		defines { "_SIMPLE", "NDEBUG" }
+		symbols "Off"
+		optimize "Full"
+		runtime "Release"
+		linktimeoptimization "On"
+
+	filter "system:windows"
+		systemversion "latest"
+		
+		flags {
+			"MultiProcessorCompile"
+		}
+
+		links {
+			"dwmapi"
+		}
+  
+	--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	project "Launcher"
+		kind "WindowedApp"
+		location (directories.Local)
+		targetname "TestEngine_%{cfg.buildcfg}"
+		fatalwarnings { "All" }
+	
+		files {
+			directories.Launcher .. "/**.h",
+			directories.Launcher .. "/**.hpp",
+			directories.Launcher .. "/**.cpp",
+			directories.Launcher .. "/**.ixx",
+		}
+
+		includedirs { 
+			directories.Launcher,
+		}
+
+	--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
