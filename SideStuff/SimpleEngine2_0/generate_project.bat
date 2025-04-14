31380669f1532bf6ae1b8b6b66c302dbc3ea6fa1
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-04-14 18:50:40 +0200

    Testing new generated bat

--- SideStuff/SimpleEngine2_0/generate_project.bat
+++ SideStuff/SimpleEngine2_0/generate_project.bat
@@ -1,2 +1,22 @@
-call "Premake/premake5" --file=premake5.lua vs2022
-set /p DUMMY=press any key to exit
+@echo off
+setlocal
+
+set PREMAKE_EXE=Premake\premake5.exe
+set BUILD_SCRIPT=premake5.lua
+set ACTION=vs2022
+
+echo Generating Visual Studio 2022 project files...
+echo.
+
+if exist "%PREMAKE_EXE%" (
+    call "%PREMAKE_EXE%" --file=%BUILD_SCRIPT% %ACTION%
+) else (
+    echo ERROR: Premake executable not found at %PREMAKE_EXE%
+    pause
+    exit /b 1
+)
+
+echo.
+echo Project files generated successfully.
+pause
+endlocal
